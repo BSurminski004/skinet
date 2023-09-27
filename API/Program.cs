34589a98035c32433470ce4b65b05de56e951e9c
Bartek.S <bsurminski27@gmail.com>
2023-09-27 21:16:27 +0200

    EndOfSetion3

--- API/Program.cs
+++ API/Program.cs
@@ -1,20 +1,26 @@
-﻿using Infrastructure.Data;
+﻿using Core.Interfaces;
+using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddControllers();
 builder.Services.AddDbContext<StoreContext>(x => x.UseSqlite(
-    builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 //Serwis ktory dodaje Swaggera,pozwala on na wyswietlanie listy dostepnych ednpointow oraz na wysylanie zapytan do nich
 builder.Services.AddSwaggerGen();
 
 //zmiena app jest instacja WebApplication, której opcje zostały skonfigurowane w linijkach wyżej 
 var app = builder.Build();
+
+//Service for automate migration applying
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -26,8 +32,22 @@ if (app.Environment.IsDevelopment())
 
 //moge uzywac przekierowania na httpsz http
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+    try{
+        var context = services.GetRequiredService<StoreContext>();
+        context.Database.Migrate();
+        await StoreContextSeed.SeedAsync(context, loggerFactory);
+    }
+    catch (Exception ex){
+        var logger = loggerFactory.CreateLogger<Program>();
+        logger.LogError(ex, "An error occured during migration!");
+    }
+}
 
 app.MapControllers();
 
--- Infrastructure/Data/Migrations/20230915115742_InitialCreate.Designer.cs
+++ Infrastructure/Data/Migrations/20230915115742_InitialCreate.Designer.cs
@@ -2,6 +2,7 @@
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Data.Migrations
 {
     [DbContext(typeof(StoreContext))]
-    partial class StoreContextModelSnapshot : ModelSnapshot
+    [Migration("20230915115742_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.10");
@@ -31,7 +34,7 @@ namespace Infrastructure.Data.Migrations
                         .HasMaxLength(100)
                         .HasColumnType("TEXT");
 
-                    b.Property<string>("PictureUrl")
+                    b.Property<string>("PoctureUrl")
                         .IsRequired()
                         .HasColumnType("TEXT");
 
